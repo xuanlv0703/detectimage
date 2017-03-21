e9cf39690918d747f49374f68c532087e584d444
xuan <xuan@rasia.info>
2017-03-21 11:18:46 +0700

    db

--- server.js
+++ server.js
@@ -51,6 +51,44 @@ app.get("/", function (req, res) {
 
 config.routes.map(addApi) // inspect the config file for details
 
+
+// app.use(function(req, res, next) {
+//     // check header or url parameters or post parameters for token
+//     var token = req.body.token || req.param.token || req.headers['x-access-token'] || req.query.token;
+
+//     if (req.url == '/api/user/login' || req.method == 'GET') {
+//         next();
+//         return;
+//     }
+
+//     // decode token
+//     if (token) {
+//         // verifies secret and checks exp
+//         jwt.verify(token, config.secret, function(err, decoded) {
+
+//             if (err) {
+//                 return res.status(403).send({
+//                     success: false,
+//                     message: 'Failed to authenticate token or token expried.'
+//                 });
+//             } else {
+//                 // if everything is good, save to request for use in other routes
+//                 req.decoded = decoded;
+//                 next();
+//             }
+//         });
+//     } else {
+
+
+//         // if there is no token
+//         // return an error
+//         return res.status(403).send({
+//             success: false,
+//             message: 'No token provided.'
+//         });
+//     }
+// });
+
 app.use(function (err, req, res, next) {
   console.error('xuanle',err.stack)
     var receiverEmail = config.errormail;
